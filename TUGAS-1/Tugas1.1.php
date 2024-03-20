5598e6f72ff0aac57a85849f0a1d1d570e83b6f2
Rommyrst <164219238+Rommyrst@users.noreply.github.com>
2024-03-21 00:17:36 +0700

    Add files via upload

---
+++ TUGAS-1/Tugas1.1.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Tambah Barang</title>
+    </head>
+    <body>
+        <form action="Tugas1.php" >
+            
+             <label form ="kode">Kode</label><br>
+             <input type ="text" id="kode" name="kode"><br><br>
+
+             <label form ="nama">Nama<label><br>
+             <input type ="text" id="nama" name="nama"><br><br>
+
+             <label form ="nama">Satuan<label><br>
+             <input type ="text" id="satuan" name="satuan"><br><br>
+
+             <label form ="nama">Beli<label><br>
+             <input type ="text" id="beli" name="beli"><br><br>
+
+             <label form ="nama">Jual<label><br>
+             <input type ="text" id="jual" name="jual"><br><br>
+
+             <input type="submit" value="Save" name="save">
+             <input type="submit" value="Cancel" name="cancel">
+
+        </form>
+    </body>
+</html>
---
+++ TUGAS-1/Tugas1.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <title>Tugas1</title>
+</head>
+<body>
+    
+    <a href="Tugas1.1.php" target="blank"> <input type = "button" name ="Tambah" value ="Tambah"; >
+    <br><br>
+
+    <table border = "3" style="width:100%; height:100px">
+        <tr>
+            <th rowspan ="2">Kode</th>
+            <th rowspan ="2">Nama</th>
+            <th rowspan ="2">Satuan</th>
+            <th colspan ="2"> Harga</th>
+            <th rowspan ="2">Action</th>
+        </tr>
+        <!-- baris 3 -->
+        <tr>
+             <th> Jual</th>
+             <th> Beli</th>
+        </tr>
+        <tr>
+            <td> M01 </td>
+            <td> CPU </td>
+            <td> pcs</td>
+            <td> 200 </td>
+            <td> 350 </td>
+            <td> 
+                <a href="Tugas1.2.php" target="blank"> <input type = "button" name ="update" value ="update"; > 
+            </td>
+        </tr>
+        <tr>
+            <td> M02 </td>
+            <td> RAM </td>
+            <td> pcs</td>
+            <td> 300 </td>
+            <td> 450 </td>
+            <td> 
+                <a href="Tugas1.2.php" target="blank"> <input type = "button" name ="update" value ="update"; >
+            </td>
+        </tr>
+        <tr>
+            <td> M03 </td>
+            <td> VGA </td>
+            <td> pcs</td>
+            <td> 400 </td>
+            <td> 550 </td>
+            <td> 
+                <a href="Tugas1.2.php" target="blank"> <input type = "button" name ="update" value ="update"; >
+            </td>
+        </tr>
+    </table>
+
+</body>
+</html>
